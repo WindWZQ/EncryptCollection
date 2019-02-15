d27380bb598eb890e2626137d7025eb2af17e31a
wangzhenqi <331472729@qq.com>
2019-02-15 14:52:13 +0800

    des ecb cbc finish,doing ede

--- app/src/main/java/com/wzq/encrypt/des/DesUtil.java
+++ app/src/main/java/com/wzq/encrypt/des/DesUtil.java
@@ -6,46 +6,55 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
  * 1.DES加密，支持ECB和CBC两种工作模式
- *   ECB：电子密本方式
- *   优点：分块处理，并行处理
- *   缺点：同样的原文得到相同的密文，容易被攻击
- *
- *   CBC：密码分组链接
- *   优点：同样的原文得到不同的密文，原文的改变影响后面全部密文
- *   缺点：加密需要串行处理，误差传递
- *
+ * <p>
+ * ECB：电子密本方式
+ * 优点：简单，有利于并行计算
+ * 缺点：不能隐藏明文的模式，容易被攻击，不安全
+ * <p>
+ * <p>
+ * CBC：密码分组链接
+ * 优点：安全性好于ECB，是SSL、IPSec的标准
+ * 缺点：不利于并行计算，误差传递
+ * <p>
+ * <p>
  * 2.DESEDE加密，三倍长的Des加密
  */
 public class DesUtil {
     private static final String DES_BASE = "DES";
 
-    public static final String DES_ECB = "DES/ECB/PKCS5Padding";
-    public static final String DES_CBC = "DES/CBC/PKCS5Padding";
+    private static final String DES_ECB = "DES/ECB/PKCS5Padding";
+    private static final String DES_CBC = "DES/CBC/PKCS5Padding";
 
-    // desede加密
     private static final String DESEDE_BASE = "DESede";
 
-    public static final byte DEFAULT_KEY[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    public static final byte DEFAULT_IV[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    //
+    public static final byte DEFAULT_KEY[] = {1, 2, 3, 4, 5, 6, 7,8};
+    public static final byte DEFAULT_IV[] = {8, 7, 6, 5, 1, 2, 3};
 
-    // des加密
-    public static byte[] encrypt(byte[] data, byte[] key, String mode) {
+    // ede秘钥必须是16或24字节
+    public static final byte DEFAULT_KEY_EDE[] = {1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 7, 8};
+
+    /**
+     * ECB加解密
+     *
+     * @param data 源数据
+     * @param key  秘钥
+     * @param mode 加密：Cipher.ENCRYPT_MODE
+     *             解密：Cipher.DECRYPT_MODE
+     * @return 加解密后的byte数组
+     */
+    public static byte[] ecb(byte[] data, byte[] key, int mode) {
         try {
-            SecureRandom random = new SecureRandom();
-            DESKeySpec desKey = new DESKeySpec(key);
-            //创建一个密匙工厂，然后用它把DESKeySpec转换成
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES_BASE);
-            SecretKey securekey = keyFactory.generateSecret(desKey);
-            //Cipher对象实际完成加密操作
-            Cipher cipher = Cipher.getInstance(mode);
-            //用密匙初始化Cipher对象
-            cipher.init(Cipher.ENCRYPT_MODE, securekey, random);
-            //现在，获取数据并加密
-            //正式执行加密操作
+            SecretKey secureKey = keyFactory.generateSecret(new DESKeySpec(key));
+            Cipher cipher = Cipher.getInstance(DES_ECB);
+            cipher.init(mode, secureKey, new SecureRandom());
+
             return cipher.doFinal(data);
         } catch (Exception e) {
             e.printStackTrace();
@@ -54,23 +63,25 @@ public class DesUtil {
         return null;
     }
 
-    // des解密
-    public static byte[] decrypt(byte[] src, byte[] key, String mode) {
+    /**
+     * CBC加解密
+     *
+     * @param data 源数据
+     * @param key  秘钥
+     * @param iv   向量
+     * @param mode 加密：Cipher.ENCRYPT_MODE
+     *             解密：Cipher.DECRYPT_MODE
+     * @return 加解密后的byte数组
+     */
+    public static byte[] cbc(byte[] data, byte[] key, byte[] iv, int mode) {
         try {
-            // DES算法要求有一个可信任的随机数源
-            SecureRandom random = new SecureRandom();
-            // 创建一个DESKeySpec对象
-            DESKeySpec desKey = new DESKeySpec(key);
-            // 创建一个密匙工厂
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES_BASE);
-            // 将DESKeySpec对象转换成SecretKey对象
-            SecretKey securekey = keyFactory.generateSecret(desKey);
-            // Cipher对象实际完成解密操作
-            Cipher cipher = Cipher.getInstance(mode);
-            // 用密匙初始化Cipher对象
-            cipher.init(Cipher.DECRYPT_MODE, securekey, random);
-            // 真正开始解密操作
-            return cipher.doFinal(src);
+            SecretKey secretKey = keyFactory.generateSecret(new DESKeySpec(key));
+            Cipher cipher = Cipher.getInstance(DES_CBC);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+            cipher.init(mode, secretKey, ivSpec);
+
+            return cipher.doFinal(data);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -79,28 +90,17 @@ public class DesUtil {
     }
 
     // desede加密方法
-    public static byte[] encryptEde(byte[] src, byte[] key) {
+    public static byte[] ede(byte[] data, byte[] key, int mode) {
         try {
             SecretKey secretKey = new SecretKeySpec(key, DESEDE_BASE);
             Cipher c1 = Cipher.getInstance(DESEDE_BASE);
-            c1.init(Cipher.ENCRYPT_MODE, secretKey);
-            return c1.doFinal(src);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+            c1.init(mode, secretKey);
 
-    // desede解密函数
-    public static byte[] decryptEde(byte[] src, byte[] key) {
-        try {
-            SecretKey secretKey = new SecretKeySpec(key, DESEDE_BASE);
-            Cipher c1 = Cipher.getInstance(DESEDE_BASE);
-            c1.init(Cipher.DECRYPT_MODE, secretKey);
-            return c1.doFinal(src);
+            return c1.doFinal(data);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return null;
     }
 
--- app/src/main/java/com/wzq/encrypt/ui/DesFragment.java
+++ app/src/main/java/com/wzq/encrypt/ui/DesFragment.java
@@ -14,6 +14,8 @@ import android.widget.Button;
 import com.wzq.encrypt.R;
 import com.wzq.encrypt.des.DesUtil;
 
+import javax.crypto.Cipher;
+
 public class DesFragment extends Fragment {
     private final String TAG = this.getClass().getSimpleName();
 
@@ -33,24 +35,30 @@ public class DesFragment extends Fragment {
 
         btn1.setOnClickListener(v -> {
             String str = "0123456789abcdefghijklmnopqrstuvwxyz";
-            Log.i(TAG, "src: " + str);
-            byte en[] = DesUtil.encrypt(str.getBytes(), DesUtil.DEFAULT_KEY, DesUtil.DES_ECB);
+            Log.i(TAG, "ecb src: " + str);
+            byte en[] = DesUtil.ecb(str.getBytes(), DesUtil.DEFAULT_KEY, Cipher.ENCRYPT_MODE);
             String enStr = Base64.encodeToString(en, Base64.DEFAULT);
             Log.i(TAG, "en: " + enStr);
-            byte de[] = DesUtil.decrypt(en, DesUtil.DEFAULT_KEY, DesUtil.DES_ECB);
+            byte de[] = DesUtil.ecb(en, DesUtil.DEFAULT_KEY, Cipher.DECRYPT_MODE);
             Log.i(TAG, "de: " + new String(de));
         });
         btn2.setOnClickListener(v -> {
             String str = "0123456789abcdefghijklmnopqrstuvwxyz";
-            Log.i(TAG, "src: " + str);
-            byte en[] = DesUtil.encrypt(str.getBytes(), DesUtil.DEFAULT_KEY, DesUtil.DES_CBC);
+            Log.i(TAG, "cbc src: " + str);
+            byte en[] = DesUtil.cbc(str.getBytes(), DesUtil.DEFAULT_KEY, DesUtil.DEFAULT_IV, Cipher.ENCRYPT_MODE);
             String enStr = Base64.encodeToString(en, Base64.DEFAULT);
             Log.i(TAG, "en: " + enStr);
-            byte de[] = DesUtil.decrypt(en, DesUtil.DEFAULT_KEY, DesUtil.DES_CBC);
+            byte de[] = DesUtil.cbc(en, DesUtil.DEFAULT_KEY, DesUtil.DEFAULT_IV, Cipher.DECRYPT_MODE);
             Log.i(TAG, "de: " + new String(de));
         });
         btn3.setOnClickListener(v -> {
-
+            String str = "0123456789abcdefghijklmnopqrstuvwxyz";
+            Log.i(TAG, "ede src: " + str);
+            byte en[] = DesUtil.ede(str.getBytes(), DesUtil.DEFAULT_KEY_EDE, Cipher.ENCRYPT_MODE);
+            String enStr = Base64.encodeToString(en, Base64.DEFAULT);
+            Log.i(TAG, "en: " + enStr);
+            byte de[] = DesUtil.ede(en, DesUtil.DEFAULT_KEY_EDE, Cipher.DECRYPT_MODE);
+            Log.i(TAG, "de: " + new String(de));
         });
 
     }
